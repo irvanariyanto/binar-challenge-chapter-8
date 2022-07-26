afeeeddfe63ec771e4dbb3882ff4e870d9830a1c
GDP-IrvanAriyanto <irvan.ariyanto@gdplabs.id>
2022-07-26 21:56:28 +0700

    create Swagger documentation

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const apiRouter = require('./server/routes')
-const errorHandler = require('./server/middlewares/errorHandler')
-const PORT = process.env.PORT || 4000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const apiRouter = require("./server/routes");
+const errorHandler = require("./server/middlewares/errorHandler");
+const PORT = process.env.PORT || 4000;
 
 // middlewares
-app.use(cors())
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
-app.use(errorHandler)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(errorHandler);
+
+const swaggerUi = require("swagger-ui-express"),
+  swaggerDocument = require("./swagger.json");
 
 /**
  * @Routes /api
  * entrypoint for all API routes
  */
-app.use("/api", apiRouter)
+app.use("/api", apiRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`)
-})
+  console.log(`Listening on http://localhost:${PORT}`);
+});
